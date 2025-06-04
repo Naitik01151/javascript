8ce57407a1090bc8af8f0c1f89af7265c6eec0c8
Guptanaitik01 <gnaitik395@gmail.com>
2025-06-04 18:45:59 +0530

    Strings

---
+++ 01_basics/06_Strings.js
@@ -0,0 +1,14 @@
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+let str1="Naitik"
+let srt2= " Hello"
+let str3 = str1+srt2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+
